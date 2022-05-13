bad8273252ed3a0ed03028acd5bbeb9db47f7edf
Smaassen2 <71068315+Smaassen2@users.noreply.github.com>
2022-05-13 14:59:10 +0200

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,8 +1,9 @@
+# accurater dan main1
 from Bio import Entrez, Medline
 
 def obtain_combinations():
     compoundlist = []
-    with open('compounds.txt', 'r') as file:
+    with open('compounds2.txt', 'r') as file:
         for line in file:
             line = line.strip()
             if line:
@@ -10,7 +11,7 @@ def obtain_combinations():
     file.close()
 
     molecular_effectslist = []
-    with open('molecular_effects.txt', 'r') as file2:
+    with open('molecular_effects2.txt', 'r') as file2:
         for line2 in file2:
             line2 = line2.strip()
             if line2:
@@ -18,23 +19,47 @@ def obtain_combinations():
     file2.close()
 
     geneslist = []
-    with open('genes.txt', 'r') as file3:
+    with open('genes2.txt', 'r') as file3:
         for line3 in file3:
             line3 = line3.strip()
             if line3:
                 geneslist.append(line3)
     file3.close()
 
+    return compoundlist, molecular_effectslist, geneslist
+
+def search(compoundlist, molecular_effectslist, geneslist):
+
     highest_occurence = 0
     highest_occurence_combination = ""
-    possible_combinations = []
+
     for compound in compoundlist:
         for molecular_effect in molecular_effectslist:
             for gene in geneslist:
-                inputterms = compound + "+" + \
-                            molecular_effect + "+" + gene
-                possible_combinations.append(inputterms)
-    print(len(possible_combinations))
+                inputterms = compound + " AND " + \
+                            molecular_effect + " AND " + gene
+                #print(inputterms)
+
+                Entrez.email = "A.N.Other@example.com"  # Always tell NCBI who you are
+                handle = Entrez.esearch(db="pubmed", term=inputterms, retmax=1500)
+                record = Entrez.read(handle)
+                handle.close()
+                #idlist = record["IdList"]
+                #print(idlist)
+                amount_of_hits = int(record["Count"])
+                if amount_of_hits > 0:
+                    print(inputterms + " --- " + str(amount_of_hits))
+
+                if amount_of_hits > highest_occurence:
+                    highest_occurence = amount_of_hits
+                    highest_occurence_combination = inputterms
+
+                elif amount_of_hits == highest_occurence:
+                    highest_occurence_combination += " and " + inputterms
+
+    print(highest_occurence_combination)
+    print(highest_occurence)
 
 if __name__ == '__main__':
-    obtain_combinations()
+    compoundlist, molecular_effectslist, geneslist = obtain_combinations()
+    search(compoundlist, molecular_effectslist, geneslist)
